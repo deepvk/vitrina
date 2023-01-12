05a5751e1f05ad5704bd63f53ab85f4616d43111
Daniel Kadochnikov <dvkadochnikov@edu.hse.ru>
2023-01-13 02:20:11 +0300

    OCR flag

--- src/datasets/vtr_dataset.py
+++ src/datasets/vtr_dataset.py
@@ -7,7 +7,7 @@ from torch.utils.data import Dataset
 
 from src.datasets.common import DatasetSample
 from src.utils.common import clean_text
-from src.utils.slicer import VTRSlicer
+from src.utils.slicer import VTRSlicer, VTRSlicerOCR
 
 
 class VTRDataset(Dataset):
@@ -20,34 +20,37 @@ class VTRDataset(Dataset):
         stride: int = 5,
         max_seq_len: int = 512,
         ratio: float = 0.7,
-        ocr_flag: bool = True,
+        ocr_flag: bool = False,
     ):
         logger.info(f"Initializing VTRDataset with {len(labeled_texts)} samples, use max seq len {max_seq_len}")
         self.labeled_texts = labeled_texts
         self.max_seq_len = max_seq_len
         self.ocr_flag = ocr_flag
 
-        self.slicer = VTRSlicer(font=font, font_size=font_size, window_size=window_size, stride=stride, ratio=ratio)
+        self.slicer_ocr = VTRSlicerOCR(font=font, font_size=font_size, window_size=window_size, stride=stride, ratio=ratio)
+        self.slicer = VTRSlicer(font=font, font_size=font_size, window_size=window_size, stride=stride)
 
     def __len__(self) -> int:
         return len(self.labeled_texts)
 
-    def __getitem__(self, index) -> tuple[Any, Any, str | int]:
+    def __getitem__(self, index) -> tuple[Any, str | int, list[str]] | tuple[Any, str | int]:
         sample = self.labeled_texts[index]
         raw_text = clean_text(sample["text"])
 
         # [n slices; font size; window size]
         if self.ocr_flag:
-            slices, slice_text = self.slicer(raw_text)
-        slices = slices[: self.max_seq_len]
-        slice_text = slice_text[: self.max_seq_len]
-
-        return slices, sample["label"], slice_text
+            slices, slice_text = self.slicer_ocr(raw_text)
+            slices = slices[: self.max_seq_len]
+            slice_text = slice_text[: self.max_seq_len]
+            return slices, sample["label"], slice_text
+        else:
+            slices = self.slicer(raw_text)
+            slices = slices[: self.max_seq_len]
+            return slices, sample["label"]
 
     def collate_function(self, batch: list[tuple[torch.Tensor, int]]) -> dict[str, torch.Tensor]:
         slices = [item[0] for item in batch]
         labels = [item[1] for item in batch]
-        texts = [item[2] for item in batch]
 
         # [batch size; most slices; font size; window size]
         batched_slices = pad_sequence(slices, batch_first=True, padding_value=0.0).float()
@@ -58,9 +61,17 @@ class VTRDataset(Dataset):
         for i, s in enumerate(slices):
             attention_mask[i, : len(s)] = 1
 
-        return {
-            "slices": batched_slices,
-            "attention_mask": attention_mask,
-            "labels": torch.tensor(labels, dtype=torch.float),
-            "texts": texts
-        }
+        if self.ocr_flag:
+            texts = [item[2] for item in batch]
+            return {
+                "slices": batched_slices,
+                "attention_mask": attention_mask,
+                "labels": torch.tensor(labels, dtype=torch.float),
+                "texts": texts
+            }
+        else:
+            return {
+                "slices": batched_slices,
+                "attention_mask": attention_mask,
+                "labels": torch.tensor(labels, dtype=torch.float),
+            }
