3bee3f8445ab73c8a85aeaf830177ab4cdcbf032
Daniel Kadochnikov <dvkadochnikov@edu.hse.ru>
2023-01-13 05:32:53 +0300

    black formatting

--- src/datasets/vtr_dataset.py
+++ src/datasets/vtr_dataset.py
@@ -50,9 +50,7 @@ class VTRDataset(Dataset):
             slices = slices[: self.max_seq_len]
             return slices, sample["label"]
 
-    def collate_function(
-        self, batch: list[tuple[torch.Tensor, int, list[str]]]
-    ) -> dict[str, torch.Tensor | list[Any]]:
+    def collate_function(self, batch: list[tuple[torch.Tensor, int, list[str]]]) -> dict[str, torch.Tensor | list[Any]]:
         slices = [item[0] for item in batch]
         labels = [item[1] for item in batch]
 
