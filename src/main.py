5efe4c5ec6f0a2eb98e6633d248afde7f19ddaf8
dkadochnikov <dvkadochnikov@edu.hse.ru>
2023-01-14 15:57:22 +0300

    add VTRDatasetOCR class

--- src/main.py
+++ src/main.py
@@ -2,10 +2,11 @@ from argparse import ArgumentParser, Namespace
 
 from loguru import logger
 from torch.nn import BCEWithLogitsLoss
+from torch.utils.data import Dataset
 
 from src.datasets.bert_dataset import BERTDataset
 from src.datasets.bert_dataset_sl import BERTDatasetSL
-from src.datasets.vtr_dataset import VTRDataset
+from src.datasets.vtr_dataset import VTRDataset, VTRDatasetOCR
 from src.datasets.vtr_dataset_sl import VTRDatasetSL
 from src.models.ttr.classifier import TokensToxicClassifier
 from src.models.ttr.sequence_labeler import TextTokensSequenceLabeler
@@ -100,44 +101,75 @@ def train_vtr_encoder(args: Namespace, train_data: list, val_data: list = None, 
     )
     criterion = BCEWithLogitsLoss()
 
-    train_dataset = VTRDataset(
-        train_data,
-        vtr.font,
-        vtr.font_size,
-        vtr.window_size,
-        vtr.stride,
-        training_config.max_seq_len,
-        vtr.ratio,
-        ocr_flag,
-    )
-    val_dataset = (
-        VTRDataset(
-            val_data,
+    if ocr_flag:
+        train_dataset: Dataset = VTRDatasetOCR(
+            train_data,
             vtr.font,
             vtr.font_size,
             vtr.window_size,
             vtr.stride,
             training_config.max_seq_len,
             vtr.ratio,
-            ocr_flag,
         )
-        if val_data
-        else None
-    )
-    test_dataset = (
-        VTRDataset(
-            test_data,
+        val_dataset: Dataset = (
+            VTRDatasetOCR(
+                val_data,
+                vtr.font,
+                vtr.font_size,
+                vtr.window_size,
+                vtr.stride,
+                training_config.max_seq_len,
+                vtr.ratio,
+            )
+            if val_data
+            else None
+        )
+        test_dataset: Dataset = (
+            VTRDatasetOCR(
+                test_data,
+                vtr.font,
+                vtr.font_size,
+                vtr.window_size,
+                vtr.stride,
+                training_config.max_seq_len,
+                vtr.ratio,
+            )
+            if test_data
+            else None
+        )
+    else:
+        train_dataset = VTRDataset(
+            train_data,
             vtr.font,
             vtr.font_size,
             vtr.window_size,
             vtr.stride,
             training_config.max_seq_len,
-            vtr.ratio,
-            ocr_flag,
         )
-        if test_data
-        else None
-    )
+        val_dataset = (
+            VTRDataset(
+                val_data,
+                vtr.font,
+                vtr.font_size,
+                vtr.window_size,
+                vtr.stride,
+                training_config.max_seq_len,
+            )
+            if val_data
+            else None
+        )
+        test_dataset = (
+            VTRDataset(
+                test_data,
+                vtr.font,
+                vtr.font_size,
+                vtr.window_size,
+                vtr.stride,
+                training_config.max_seq_len,
+            )
+            if test_data
+            else None
+        )
 
     train(
         model,
