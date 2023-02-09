ba29b5fe587b0a1bd01399ceffcd07989d593ac0
kategimranova <kate.gimranova@mail.ru>
2023-02-09 13:18:30 +0300

    added char2array

--- src/utils/slicer.py
+++ src/utils/slicer.py
@@ -75,20 +75,18 @@ class VTRSlicerWithText:
 class VTRSlicer:
     def __init__(
         self,
-        window_size: int = 25,
-        stride: int = 10,
-        font: str = "fonts/NotoSans.ttf",
-        font_size: int = 15,
+        char2array: dict,
+        window_size: int = 32,
+        stride: int = 5,
     ):
         logger.info(f"Init VTRSlicer | window_size={window_size}, stride={stride}, font={font}, font_size={font_size}")
         self.window_size = window_size
         self.stride = stride
-        self.font = font
-        self.font_size = font_size
+        self.char2array = char2array
 
     def __call__(self, text: str, max_slice_count: int = None) -> torch.Tensor:
-        image = text2image(text, font=self.font, font_size=self.font_size)
-        image_bytes = torch.as_tensor(np.array(image))
+        unknown_token = self.char2array["UNK"]
+        image_bytes = torch.Tensor(np.concatenate([self.char2array.get(char, unknown_token) for char in text], axis=1))
 
         image_width = image_bytes.shape[1]
         padded_image_width = int(
@@ -96,8 +94,7 @@ class VTRSlicer:
             + self.window_size
         )
 
-        image_bytes = F.pad(image_bytes, (0, padded_image_width - image_width), "constant", 255)
-        image_bytes = 255 - image_bytes
+        image_bytes = F.pad(image_bytes, (0, padded_image_width - image_width), "constant", 0)
         slices = image_bytes.unfold(1, self.window_size, self.stride).permute((1, 0, 2))[:max_slice_count]
 
         return slices
