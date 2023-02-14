101fde6cca8d5de54aca5a8f75f851a164df8c8b
kategimranova <kate.gimranova@mail.ru>
2023-02-14 13:55:21 +0300

    move char2array to main

--- src/utils/config.py
+++ src/utils/config.py
@@ -14,7 +14,6 @@ class VTRConfig:
     hidden_size_ocr: int
     num_layers_ocr: int
     ratio: float
-    char2array: str
     max_slices_count_per_word: int = None
 
     @classmethod
@@ -38,14 +37,6 @@ class VTRConfig:
         arg_parser.add_argument(
             "--out-channels", type=int, default=256, help="Number of output channels in the last convolutional layer."
         )
-
-        arg_parser.add_argument(
-            "--char2array",
-            type=str,
-            default="resources/char2array.pkl",
-            help="Path to char2array [only for VTR model].",
-        )
-
         arg_parser.add_argument("--hidden-size-ocr", type=int, default=256, help="Hidden size to use for OCR.")
         arg_parser.add_argument("--num-layers-ocr", type=int, default=2, help="Number of layers to use in OCR.")
         arg_parser.add_argument("--ratio", type=float, default=0.7, help="Ratio of letter to be detected on a slice.")
