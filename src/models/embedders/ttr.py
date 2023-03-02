68aa49129f2f992593437fb140f8fc134d587ea5
Daniel Kadochnikov <dvkadochnikov@edu.hse.ru>
2023-03-02 06:46:33 +0300

    ttr embedding

--- src/models/embedders/ttr.py
+++ src/models/embedders/ttr.py
@@ -5,6 +5,9 @@ class TTREmbedder(nn.Module):
     def __init__(self, num_embeddings, emb_size):
         super().__init__()
 
+        self.embedder = nn.Embedding(num_embeddings, emb_size)
+
     def forward(self, batch):
-        output = {"embeddings": [batch["input_ids"], batch["attention_mask"], batch["token_type_ids"]]}
+        output = {"embeddings": self.embedder(batch["input_ids"])}
+
         return output
