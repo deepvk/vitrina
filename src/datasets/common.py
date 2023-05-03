e4c9ae3da75a2cebe27ecf80ffe2cd4a74ed9d01
Ekaterina Gimranova <kate.gimranova@mail.ru>
2023-05-03 18:33:27 +0300

    added Augmentation and Slices dataset

--- src/datasets/common.py
+++ src/datasets/common.py
@@ -1,4 +1,11 @@
+import torch
 from typing import TypedDict
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import IterableDataset
+
+from src.datasets.translation_datasets import NLLBDataset
+from src.utils.augmentation import TextAugmentationWrapper
+from src.utils.slicer import VTRSlicer
 
 
 class DatasetSample(TypedDict):
@@ -19,3 +26,70 @@ class SLDatasetSample(TypedDict):
 
     text: list[tuple[str, int]]
     label: int
+
+
+def collate_batch_common(slices: list[torch.Tensor], labels: list[int]):
+    # [batch size; most slices; font size; window size]
+    batched_slices = pad_sequence(slices, batch_first=True, padding_value=0.0).float()
+    bs, ms, _, _ = batched_slices.shape
+
+    # [batch size; most slices]
+    attention_mask = torch.zeros((bs, ms), dtype=torch.float)
+    for i, s in enumerate(slices):
+        attention_mask[i, : len(s)] = 1
+
+    return {
+        "slices": batched_slices,
+        "attention_mask": attention_mask,
+        "labels": torch.tensor(labels, dtype=torch.float),
+    }
+
+
+class AugmentationDataset(IterableDataset):
+    def __init__(self, dataset: NLLBDataset, leet_symbols: dict, cluster_symbols: dict, proba_per_text: float):
+        self.dataset = dataset
+        self.augmentation = TextAugmentationWrapper(
+            leet_symbols=leet_symbols, cluster_symbols=cluster_symbols, proba_per_text=proba_per_text
+        )
+
+    def __iter__(self):
+        iterator = iter(self.dataset)
+        while True:
+            try:
+                text, label = next(iterator)
+            except StopIteration:
+                return
+            else:
+                noisy_text = self.augmentation(text)
+                yield noisy_text, label
+
+    def get_num_classes(self):
+        return self.dataset.get_num_classes()
+
+
+class SlicesDataset(IterableDataset):
+    def __init__(
+        self, dataset: NLLBDataset, char2array: dict, window_size: int = 32, stride: int = 5, max_seq_len: int = 512
+    ):
+        self.dataset = dataset
+        self.max_seq_len = max_seq_len
+        self.slicer = VTRSlicer(char2array=char2array, window_size=window_size, stride=stride)
+
+    def __iter__(self):
+        iterator = iter(self.dataset)
+        while True:
+            try:
+                text, label = next(iterator)
+            except StopIteration:
+                return
+            else:
+                slices = self.slicer(text)
+                slices = slices[: self.max_seq_len]
+                yield slices, label
+
+    def get_num_classes(self):
+        return self.dataset.get_num_classes()
+
+    def collate_function(self, batch: list[tuple[torch.Tensor, int]]) -> dict[str, torch.Tensor]:
+        slices, labels = [list(item) for item in zip(*batch)]
+        return collate_batch_common(slices, labels)
