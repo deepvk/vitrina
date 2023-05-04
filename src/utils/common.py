8bf3165894a55b5d30e0a71a7de205197bdd5c55
Daniel Kadochnikov <dvkadochnikov@edu.hse.ru>
2023-05-04 05:20:42 +0300

    positional encoding change

--- src/utils/common.py
+++ src/utils/common.py
@@ -186,21 +186,13 @@ class BceLossForTokenClassification(nn.Module):
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, hidden_size: int, dropout=0.1, max_len=512):
+    def __init__(self, hidden_size: int, max_len=512):
         super(PositionalEncoding, self).__init__()
 
-        #self.dropout = nn.Dropout(p=dropout)
-
-        self.pe = nn.Parameter(torch.zeros(1, max_len, hidden_size))
-        # position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
-        # div_term = torch.pow(
-        #     1e4, -torch.arange(0, hidden_size, 2).float() / hidden_size
-        # )
-        # pe = torch.zeros(max_len, 1, hidden_size)
-        # pe[:,0,0::2] = torch.sin(position * div_term)
-        # pe[:,0,1::2] = torch.cos(position * div_term)
-        # self.register_buffer("pe", pe)
+        self.pos_emb = nn.Embedding(max_len, hidden_size)
+        self.register_buffer("positions", torch.arange(max_len, dtype=torch.long).reshape(1, -1))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = x + self.pe[:, : x.size(1), :]
+        pos_emb = self.pos_emb(self.positions[:, : x.shape[1]])  # [1, seq_len, hidden_size]
+        x = x + pos_emb
         return x
