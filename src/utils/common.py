077fc0665cee14336e65a470129f3e4b53981bda
Daniel Kadochnikov <dvkadochnikov@edu.hse.ru>
2023-05-09 04:32:11 +0300

    delete empty array check

--- src/utils/common.py
+++ src/utils/common.py
@@ -129,8 +129,6 @@ def compute_ctc_loss(
     #target_lengths = pad_sequence([torch.from_numpy(get_len(arr)) for arr in texts], batch_first=True, padding_value=0)
     target_lengths = []
     for arr in texts:
-        if not len(arr):
-            continue
         target_lengths.append(torch.from_numpy(get_len(arr)))
     target_lengths = torch.cat(target_lengths, dim=0)
     ctc_loss = criterion(log_probs, targets, input_lengths, target_lengths)
