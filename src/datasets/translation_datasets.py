90c5bcb91447b22e4abb8b296a15c7980e921669
Ekaterina Gimranova <kate.gimranova@mail.ru>
2023-05-17 19:01:18 +0300

    Update translation_datasets.py

--- src/datasets/translation_datasets.py
+++ src/datasets/translation_datasets.py
@@ -1,11 +1,10 @@
 import numpy as np
-import torch
 from datasets import load_dataset
 from src.utils.common import clean_text
 from torch.utils.data import IterableDataset, Dataset
 from tqdm import tqdm
-
-from transformers import NllbTokenizer
+import os
+from src.utils.common import load_json
 
 
 class NLLBDataset(IterableDataset):
@@ -13,8 +12,6 @@ class NLLBDataset(IterableDataset):
         self,
         probas: dict,
         k: float = 0.3,
-        tokenizer: NllbTokenizer = None,
-        max_seq_len: int = None,
     ):
         self.datasets = dict()
         self.pairs = list(probas.keys())
@@ -34,11 +31,6 @@ class NLLBDataset(IterableDataset):
 
         sum_probas = sum(self.probas)
         self.probas = [prob / sum_probas for prob in self.probas]
-
-        if tokenizer:
-            self.tokenizer = tokenizer
-        if max_seq_len:
-            self.max_seq_len = max_seq_len
 
     def get_num_classes(self):
         return len(self.lang2label)
@@ -62,33 +54,18 @@ class NLLBDataset(IterableDataset):
                 label = self.lang2label[elem[0]]
                 yield text, label
 
-    def collate_function(self, batch: list[tuple[str, int]]) -> dict[str, torch.Tensor]:
-        texts = [item[0] for item in batch]
-        labels = [item[1] for item in batch]
-
-        tokenized_batch = self.tokenizer(
-            texts,
-            add_special_tokens=True,
-            max_length=self.max_seq_len,
-            truncation=True,
-            padding=True,
-            return_tensors="pt",
-        )
-        tokenized_batch["labels"] = torch.tensor(labels, dtype=torch.int64)
-
-        return tokenized_batch
-
 
 class FloresDataset(Dataset):
-    def __init__(
-        self,
-        lang2label: dict,
-        split="dev",
-    ):
+    def __init__(self, lang2label: dict, split="dev", dataset_dir: str = None):
         assert split in ["dev", "devtest"], "Split for FLORES dataset must be dev or devtest"
         self.lang2label = lang2label
         self.langs = lang2label.keys()
-        self.dataset = load_dataset("facebook/flores", "all")[split]
+
+        if dataset_dir:
+            self.dataset = load_dataset("json", data_files=os.path.join(dataset_dir, f"{split}.jsonl"))["train"]
+        else:
+            self.dataset = load_dataset("facebook/flores", "all")[split]
+
         self.data = []
         for lang in self.langs:
             column_name = f"sentence_{lang}"
@@ -98,7 +75,7 @@ class FloresDataset(Dataset):
                 text = clean_text(sentence)
                 self.data.append({"text": text, "label": current_label})
 
-    def get_dataset(self):
+    def get_data(self):
         return self.data
 
     def __getitem__(self, index) -> tuple[str, int]:
