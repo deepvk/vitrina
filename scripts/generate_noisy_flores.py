2e51880c10befbd74449830cb1978b999995361c
Ekaterina Gimranova <kate.gimranova@mail.ru>
2023-05-17 18:02:17 +0300

    Update generate_noisy_flores.py

--- scripts/generate_noisy_flores.py
+++ scripts/generate_noisy_flores.py
@@ -3,15 +3,14 @@ import json
 import os
 import pickle
 
-from src.datasets.translation_datasets import FloresDataset
 from src.utils.augmentation import TextAugmentationWrapper, init_augmentations
 from src.utils.common import save_json
 from src.utils.config import AugmentationConfig
 from tqdm import tqdm
+from datasets import load_dataset
 
 
 def add_noise_flores(
-    probas_path: str,
     leet,
     clusters,
     expected_changes_per_word,
@@ -23,22 +22,11 @@ def add_noise_flores(
 ):
     assert split in ["dev", "devtest"], "Split for FLORES dataset must be dev or devtest"
 
-    with open(probas_path, "rb") as f:
-        probas = pickle.load(f)
-
-    pairs = list(probas.keys())
-    lang2label: dict = {}
-    label2lang: dict = {}
-    label = 0
-    for pair in pairs:
-        for lang in pair.split("-"):
-            if lang not in lang2label:
-                lang2label[lang] = label
-                label2lang[label] = lang
-                label += 1
-
-    flores = FloresDataset(lang2label, split)
-    flores_data = flores.get_dataset()
+    save_dir = os.path.join(
+        save_dir, f"w{expected_changes_per_word}t{expected_changes_per_text}m{max_augmentations}p{proba_per_text}"
+    )
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
 
     with open(leet) as json_file:
         leet_symbols = json.load(json_file)
@@ -57,33 +45,20 @@ def add_noise_flores(
         max_augmentations=max_augmentations,
     )
 
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+    dataset = load_dataset("facebook/flores", "all")
+    data = []
+    for split in dataset.keys():
+        for sample in tqdm(dataset[split]):
+            for k, v in sample.items():
+                if k.startswith("sentence"):
+                    sample[k] = augmentation_wrapper(v)
+            data.append(sample)
 
-    noisy_flores = []
-    for elem in tqdm(flores_data):
-        text = elem["text"]
-        noisy_text = augmentation_wrapper(text)
-        noisy_flores.append({"text": noisy_text, "label": elem["label"]})
-
-    save_json(
-        noisy_flores,
-        os.path.join(
-            save_dir,
-            f"flores_{split}_w{expected_changes_per_word}t{expected_changes_per_text}m{max_augmentations}p{proba_per_text}.jsonl",
-        ),
-    )
-    return noisy_flores
+        save_json(data, os.path.join(save_dir, f"{split}.jsonl"))
 
 
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument(
-        "--probas-path",
-        type=str,
-        default="resources/nllb/probas_nllb.pkl",
-        help="Path to probabilities of language pairs [for lang detect task].",
-    )
 
     arg_parser.add_argument(
         "--save-dir",
