f409f8f853a4c1c8a81e1a1ce1f3fd72f8ef787e
Ekaterina Gimranova <kate.gimranova@mail.ru>
2023-05-17 09:32:31 +0300

    added save_every

--- src/utils/config.py
+++ src/utils/config.py
@@ -91,6 +91,7 @@ class TrainingConfig:
     device: str = None
     random_state: int = 21
     log_every: int = 1000
+    save_every: int = 20000
     num_workers: int = 1
 
     no_average: bool = False
@@ -116,6 +117,7 @@ class TrainingConfig:
         )
         arg_parser.add_argument("--random-state", type=int, default=21, help="Random state.")
         arg_parser.add_argument("--log-every", type=int, default=1000, help="Log every N steps.")
+        arg_parser.add_argument("--save-every", type=int, default=20000, help="Save model every N steps.")
         arg_parser.add_argument("--num-workers", type=int, default=1, help="Number of workers for data loaders.")
         arg_parser.add_argument(
             "--no-average", action="store_true", help="Do not use averaging for evaluation metrics."
